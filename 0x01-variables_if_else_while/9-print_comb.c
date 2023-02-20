0e20e23eb8a03a9f319e42bf9d3150b4ae9fc9a0
Horma9377 <jessicaamadi71@gmail.com>
2023-02-20 12:46:15 -0800

    9-prints_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,20 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
-	{
-		putchar(i + '0');
-	}
-	putchar('\n');
-	return (0);
+for (i = 0 ; i < 10 ; i++)
+{
+putchar(i + '0');
+if (i != 9)
+
+{
+putchar(',');
+putchar(' ');
+
+}
+
+}
+
+putchar('\n');
+return (0);
 }
 
